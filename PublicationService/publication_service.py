729c169e906353f00966c4bc62586ca9bbf0d11e
freon-lunarion <antonio.daniswara@gmail.com>
2018-05-24 17:54:46 +1000

    create resources "greens" for green status url : /greens/<year>

--- PublicationService/publication_service.py
+++ PublicationService/publication_service.py
@@ -149,6 +149,27 @@ def consumption(country, year):
     response = jsonify(population_response)
     response.headers._list.append(('Access-Control-Allow-Origin', '*'))
     return response, 200
+@app.route("/greens/<year>", methods=["GET"])
+def greeness(year):
+    connect(
+        db="comp9321ass3",
+        username="admin",
+        password="admin",
+        host="ds117540.mlab.com",
+        port=17540
+    )
+    result = list()
+    for data in EnergyMix.objects(year=year):
+
+        green_point = round(100 * (data.geothermal + data.hydro + data.solar + data.wind) / data.total_energy)
+        result.append({
+            'country': data.country,
+            'score': green_point
+        })
+
+    response = jsonify(result)
+    response.headers._list.append(('Access-Control-Allow-Origin', '*'))
+    return response, 200
 
 
 @app.route("/recaps/<country>/<year>", methods=["GET"])
@@ -161,23 +182,33 @@ def recaps(country,year):
         port=17540
     )
 
-    if not EnergyReport.objects(country=country, year=year):
+    if not EnergyReport.objects(country=country,year=year):
         mix(country, year)
     result = None
-    for base in EnergyReport.objects(country=country, year=year):
+    for base in EnergyReport.objects(country=country,year=year):
         sources = list()
 
         for source in base.production_source:
-            percent = round(source.amount/base.production_amount, 2)
+            percent = round(source.amount/base.production_amount * 100,2)
             sources.append({
                 'type': source.energy_type,
                 'amount': source.amount,
                 'percent': percent
             })
 
-        cons_per = base.consumption_amount / (base.population * base.energy_access / 100)
+        cons_per = base.consumption_amount * 1000000 / (base.population * base.energy_access / 100)
 
+        year_next = int(year) + 1
+        year_prev = int(year) - 1
 
+        next_report = None
+        prev_report = None
+
+        if EnergyMix.objects(country=country, year=year_next):
+            next_report = "/".join(('/recaps', base.country, str(year_next)))
+
+        if EnergyMix.objects(country=country, year=year_prev):
+            prev_report = "/".join(('/recaps', base.country, str(year_prev)))
 
         delta = base.production_amount - base.consumption_amount
         result = {
@@ -185,12 +216,24 @@ def recaps(country,year):
             "year": base.year,
             "population": base.population,
             "energy_access": base.energy_access,
-            "consumption": base.consumption_amount,
-            "production": base.production_amount,
+            "consumption": {
+                "amount": base.consumption_amount,
+                "unit": "Gwh",
+            },
+            "consumption_percapita": {
+                "amount" : cons_per,
+                "unit"   : "Kwh",
+            },
+            "production":{
+                "amount" : base.production_amount,
+                "unit": "Gwh",
+            },
             "sources": sources,
             "delta_energy": delta,
-            "consumption_percapita": cons_per,
-
+            "link": {
+                "prev": prev_report,
+                "next": next_report
+            }
 
         }
     if result is not None:
@@ -219,6 +262,9 @@ def countries_list(parameter):
     response = jsonify(ls)
     response.headers._list.append(('Access-Control-Allow-Origin', '*'))
     return response, 200
+
+
+
 
 
 if __name__ == "__main__":
